4380a8065c3cbb89d55368bf04d203f1801ea3e8
SimonVanMello <simon.vanmello@proton.me>
2024-07-12 20:01:49 +0200

    Styling

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -24,12 +24,12 @@ const Modal = (props: PropsWithChildren<Props>) => {
 
   return (
     <Dialog open={isOpen} onClose={onClose} as="div" className="relative z-50 focus:outline-none">
-      <DialogBackdrop className="fixed inset-0 bg-stone-950/30" />
+      <DialogBackdrop className="fixed inset-0 bg-stone-950/80" />
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
         <div className="flex min-h-full items-center justify-center p-4">
           <DialogPanel
             transition
-            className="w-full max-w-md rounded-xl bg-stone-200/5 p-6 backdrop-blur-2xl duration-200 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
+            className="w-full max-w-md rounded-xl bg-stone-500/5 p-6 backdrop-blur-2xl ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0 shadow-xl border border-neutral-900"
           >
             <div className="flex flex-between mb-4">
               <DialogTitle as="h2" className="text-base/7 font-medium text-white">
